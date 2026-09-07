35cad5d08bf4919883aaab8f1abf383908707425
Valleyball Jac <jac@valleyball.demo>
2026-09-06 20:54:57 -0700

    Set Vite base URL for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import capturePlugin from './vite-plugin-capture.js';
 
 export default defineConfig({
+  base: '/Valleyball-Versus/',
   server: { port: 5173, strictPort: true },
   plugins: [capturePlugin()],
 });
